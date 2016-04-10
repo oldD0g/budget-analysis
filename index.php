5c1bcfcc49a07e0a9cd0fd89661558a29c03a35f
oldD0g <oldD0g@users.noreply.github.com>
2016-04-10 16:57:09 +1000

    Update index.php to more generic version

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
         "http://www.w3.org/TR/html4/loose.dtd">
         <html>
 <head>
-        <title>Ivan's Expenditure Analysis System</title>
+        <title>Budget Expenditure Analysis System</title>
 </head>
 <body>
-<h1>Ivan's Expenditure Analysis System</h1>
+<h1>Budget Expenditure Analysis System</h1>
 
 <?php
 
