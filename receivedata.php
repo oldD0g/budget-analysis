8be97a804527f3ad22cf3bb7e2be4177d86661f9
oldD0g <oldD0g@users.noreply.github.com>
2017-06-11 09:30:47 +1000

    Added bootstrap link - slowly incorporating bootstrap code

--- receivedata.php
+++ receivedata.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-      <meta charset="utf-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"><link href="BudgetStyle.css" rel="stylesheet" type="text/css" media="screen">
